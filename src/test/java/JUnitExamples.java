2e0e8a9c6a2550c436f18cc547c98f04babf2f0e
SubkhankulovaK <ck.105@mail.ru>
2023-03-04 13:21:06 +0300

    added JUnitExamples

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitExamples {
+
+    @BeforeAll
+    static  void beforeAll() {
+        System.out.println("Here is beforeAll()");
+    }
+
+    @AfterAll
+    static  void afterAll() {
+        System.out.println("Here is afterAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Here is beforeEach()");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("    Here is afterEach()");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("        Here is firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("        Here is secondTest()");
+    }
+}
